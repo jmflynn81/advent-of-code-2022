a72d5eb51f6ec684e208e49f9f4ce2c41dcc29a4
Jonathan Flynn <jonathan.flynn@sainsburys.co.uk>
2022-12-06 08:57:35 +0000

    Day6

---
+++ 06/Question1/result.js
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+
+const syncReadFile = (filename) => {
+  const contents = readFileSync(filename, "utf-8");
+  return contents.split("\n")[0];
+};
+
+const transmission = syncReadFile("../input.txt");
+
+const starterMarker = 4;
+let found = false;
+let posistion = 0;
+
+for (let i = starterMarker; found !== true; i++) {
+  const tranSetSize = new Set([...transmission.slice(i - starterMarker, i)])
+    .size;
+  if (tranSetSize == starterMarker) {
+    posistion = i;
+    found = true;
+  }
+}
+
+console.log({ posistion });
